80da17593ac6646996d5684488f33435a8fb9dea
Malo Thebault <malothebault@lilo.org>
2023-09-11 20:52:30 +0200

    🧪 Matter.jsx is working !

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,41 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Eprouvette',
-			social: {
-				github: 'https://github.com/withastro/starlight',
+	integrations: [starlight({
+		title: 'Eprouvette',
+		logo: {
+			src: './src/assets/logo_v2.svg',
+		},
+		social: {
+			github: 'https://github.com/withastro/starlight'
+		},
+		defaultLocale: 'root',
+		// optional
+		locales: {
+			root: {
+				label: 'Français',
+				lang: 'fr' // lang is required for root locales
 			},
-			defaultLocale: 'root', // optional
-			locales: {
-				root: {
-					label: 'Français',
-					lang: 'fr', // lang is required for root locales
-				},
-				en: {
-					label: 'English',
-					lang: 'en',
-				},
-			},
-			sidebar: [
-				{
-					label: 'Physique',
-					autogenerate: { directory: 'physique' },
-				},
-				{
-					label: 'Chimie',
-					autogenerate: { directory: 'chimie' },
-				},
-			],
-		}),
-	],
+
+			en: {
+				label: 'English',
+				lang: 'en'
+			}
+		},
+		sidebar: [{
+			label: 'Physique',
+			autogenerate: {
+				directory: 'physique'
+			}
+		}, {
+			label: 'Chimie',
+			autogenerate: {
+				directory: 'chimie'
+			}
+		}]
+	}), react()]
 });
